9dc13ce522f64f7f83d010831b974437a395b9c5
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-03-08 00:03:06 +0900

    Removed top bar.

--- app/src/main/java/com/kohdev/viderex/MainActivity.java
+++ app/src/main/java/com/kohdev/viderex/MainActivity.java
@@ -1,34 +1,53 @@
 package com.kohdev.viderex;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.Window;
 import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.Camera2Renderer;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
     private CameraBridgeViewBase mOpenCvCameraView;
     public CameraBridgeViewBase.CvCameraViewListener2 camListener;
+//    private CameraDevice mCameraDevice;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.MainCameraView);
         Log.e("verify", String.valueOf(OpenCVLoader.initDebug()));
+        mOpenCvCameraView.setMaxFrameSize(1920,1080);
     }
+
+
 
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -57,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void run() {
-        Log.e("entered","hello there");
+        Log.e("entered", "hello there");
         camListener = new CameraBridgeViewBase.CvCameraViewListener2() {
 
             @Override
@@ -67,12 +86,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCameraViewStarted(int width, int height) {
-                // TODO Auto-generated method stub
+
             }
 
             @Override
             public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-                Log.e("RUNNING","gray");
                 Mat rgb = inputFrame.rgba();
                 Mat gray = new Mat();
                 Imgproc.cvtColor(rgb, gray, Imgproc.COLOR_RGB2GRAY);
@@ -82,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(camListener);
+        mOpenCvCameraView.setMaxFrameSize(1920,1080);
 
 
     }
