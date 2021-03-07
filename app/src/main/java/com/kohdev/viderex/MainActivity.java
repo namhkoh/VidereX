9b7760d20d3941a24bc62d5211e8b20ff199ada0
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-03-07 22:46:38 +0900

    First commit.

---
+++ app/src/main/java/com/kohdev/viderex/MainActivity.java
@@ -0,0 +1,89 @@
+package com.kohdev.viderex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+    private CameraBridgeViewBase mOpenCvCameraView;
+    public CameraBridgeViewBase.CvCameraViewListener2 camListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.MainCameraView);
+        Log.e("verify", String.valueOf(OpenCVLoader.initDebug()));
+    }
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i("TAG", "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    System.loadLibrary("libopencv_java3"); // if you are working with JNI
+//                    run();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mOpenCvCameraView.enableView();
+        run();
+    }
+
+    private void run() {
+        Log.e("entered","hello there");
+        camListener = new CameraBridgeViewBase.CvCameraViewListener2() {
+
+            @Override
+            public void onCameraViewStopped() {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                Log.e("RUNNING","gray");
+                Mat rgb = inputFrame.rgba();
+                Mat gray = new Mat();
+                Imgproc.cvtColor(rgb, gray, Imgproc.COLOR_RGB2GRAY);
+                return gray;
+            }
+        };
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(camListener);
+
+
+    }
+}
