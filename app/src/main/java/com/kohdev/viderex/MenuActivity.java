c0acbc7d6442a1ef2baef00dff658dbe760d98c0
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-03-13 17:23:35 +0900

    Added image capture.

--- app/src/main/java/com/kohdev/viderex/MenuActivity.java
+++ app/src/main/java/com/kohdev/viderex/MenuActivity.java
@@ -1,26 +1,48 @@
 package com.kohdev.viderex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * This activity will provide hte user with options to either follow a route or record a route.
  */
 public class MenuActivity extends AppCompatActivity {
 
+    Uri fileUri;
+    final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 2;
+    final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 3;
+
+    public static Bitmap bitmap;
+
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        checkPermission();
+        checkPermissions();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
@@ -53,8 +75,10 @@ public class MenuActivity extends AppCompatActivity {
      * This method will launch the single match method.
      */
     private void launchSingleMatch() {
-        Intent intent = new Intent(this, SingleMatchActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //fileUri = getOutputMediaFileUri(1); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
     /**
@@ -86,6 +110,69 @@ public class MenuActivity extends AppCompatActivity {
                     Uri.parse("package:" + getPackageName()));
             startActivity(intent);
             finish();
+        }
+    }
+
+
+    public void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        }
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        }
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA:
+
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("Permission status: ", "Success");
+                } else {
+                    Log.e("Permission status: ", "Failed");
+                }
+                return;
         }
     }
 
