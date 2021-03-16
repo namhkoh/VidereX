140a7cd811c6ae849a4bc91ff063e92608ae9bc4
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-03-16 22:05:41 +0900

    Adjusting the single match.

--- app/src/main/java/com/kohdev/viderex/SingleMatchActivity.java
+++ app/src/main/java/com/kohdev/viderex/SingleMatchActivity.java
@@ -7,15 +7,23 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import static org.opencv.core.CvType.CV_8UC1;
 
 /**
  * This activity will handle the Single matching ability between the current view and the image taken
@@ -28,13 +36,42 @@ public class SingleMatchActivity extends AppCompatActivity implements CameraBrid
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        OpenCVLoader.initDebug();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_single_match);
 
         mOpenCvCameraView = findViewById(R.id.OpenCVCamera);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
         Log.e("verify", String.valueOf(OpenCVLoader.initDebug()));
     }
+
+//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case LoaderCallbackInterface.SUCCESS:
+//                {
+//                    Log.i("OpenCV", "OpenCV loaded successfully");
+//                } break;
+//                default:
+//                {
+//                    super.onManagerConnected(status);
+//                } break;
+//            }
+//        }
+//    };
+
+//    private void getGoalImage(){
+//        Utils.bitmapToMat(MenuActivity.bitmap, temp);
+//        // Convert from RGB to grayscale and histeq
+//        Imgproc.cvtColor(temp, temp, Imgproc.COLOR_RGB2GRAY);
+//        //Imgproc.equalizeHist(temp, temp);
+//        Core.flip(temp, temp, 1);
+//        Size size = new Size(150,250);
+//        Imgproc.resize(temp, temp, size);
+//    }
+
 
     @Override
     public void onSensorChanged(SensorEvent event) {
@@ -59,7 +96,12 @@ public class SingleMatchActivity extends AppCompatActivity implements CameraBrid
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat frame = inputFrame.rgba();
-        //Imgproc.cvtColor(frame,frame,Imgproc.COLOR_RGBA2GRAY);
+        Imgproc.cvtColor(frame,frame,Imgproc.COLOR_RGBA2GRAY);
+        Size size = new Size(150,250);
+        Imgproc.resize(frame, frame, size);
+
+//        computeAbsDiff(temp,frame);
+
         return frame;
     }
 
@@ -70,6 +112,8 @@ public class SingleMatchActivity extends AppCompatActivity implements CameraBrid
         if (!OpenCVLoader.initDebug()) {
             Log.e("Error", "There is something wrong with OpenCV");
         } else {
+            Mat temp = new Mat();
+//            getGoalImage();
             mOpenCvCameraView.enableView();
         }
     }
@@ -89,5 +133,28 @@ public class SingleMatchActivity extends AppCompatActivity implements CameraBrid
         if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
         }
+    }
+
+    /**
+     * This method will compute the absolute difference of two images.
+     *
+     * @param current
+     * @param goal
+     * @return error
+     */
+    private static Mat computeAbsDiff(Mat current, Mat goal) {
+        int w = current.width();
+        int h = current.height();
+        Mat error = Mat.zeros(w, h, CV_8UC1);
+        if ((goal != null) && (current != null)) {
+            if ((current.height() == goal.height()) && (current.width() == goal.width()) && (current.type() == CV_8UC1) && (goal.type() == CV_8UC1)) {
+
+                // Calculate per-pixel absolute differences of current and goal images
+                Core.absdiff(current, goal, error);
+
+            }
+        }
+        Log.e("ERROR", String.valueOf(error));
+        return error;
     }
 }
