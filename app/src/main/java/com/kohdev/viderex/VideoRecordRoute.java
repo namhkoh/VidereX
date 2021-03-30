e752cf8de4d04fbe467605d856102e34b836eff5
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-03-30 18:21:41 +0900

    Added video structure feed.

--- app/src/main/java/com/kohdev/viderex/VideoRecordRoute.java
+++ app/src/main/java/com/kohdev/viderex/VideoRecordRoute.java
@@ -4,20 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
+//import org.bytedeco.javacv.AndroidFrameConverter;
+//import org.bytedeco.javacv.FFmpegFrameGrabber;
+//import org.bytedeco.javacv.Frame;
+//import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
@@ -27,14 +38,32 @@ public class VideoRecordRoute extends AppCompatActivity {
     VideoView videoView;
     String absPath;
     Uri videoUri;
+    InputStream videoStream;
+
     static final int REQUEST_VIDEO_CAPTURE = 1;
     static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("verify", String.valueOf(OpenCVLoader.initDebug()));
         setContentView(R.layout.activity_video_record_route);
         videoView = (VideoView) findViewById(R.id.routeVideo);
+        Button getFrames = findViewById(R.id.extractFramesButton);
+
+        getFrames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                try {
+//                    extractImageFrame(videoUri);
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                } catch (FFmpegFrameGrabber.Exception e) {
+//                    e.printStackTrace();
+//                }
+            }
+        });
+
         dispatchTakeVideoIntent();
     }
 
@@ -93,7 +122,7 @@ public class VideoRecordRoute extends AppCompatActivity {
                 break;
             case 1:
                 Log.e("Capture", "Success!");
-                InputStream videoStream = null;
+                videoStream = null;
                 try {
                     videoStream = getContentResolver().openInputStream(videoUri);
                 } catch (FileNotFoundException e) {
@@ -104,6 +133,26 @@ public class VideoRecordRoute extends AppCompatActivity {
                 videoView.start();
         }
     }
+
+//    private void extractImageFrame(Uri uriPath) throws FileNotFoundException, FFmpegFrameGrabber.Exception {
+//        InputStream inputStream = new FileInputStream(String.valueOf(uriPath));
+//        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputStream);
+//        AndroidFrameConverter convertToBitmap = new AndroidFrameConverter();
+//        OpenCVFrameConverter.ToMat converterToMat = new OpenCVFrameConverter.ToMat();
+//
+//        grabber.start();
+//        Mat img = new Mat();
+//
+//        for (int frameCount = 0; frameCount < grabber.getLengthInVideoFrames(); frameCount++) {
+//            Frame nthFrame = grabber.grabImage();
+//            Bitmap bitmap = convertToBitmap.convert(nthFrame);
+//            Mat mat = converterToMat.convertToOrgOpenCvCoreMat(nthFrame);
+//            System.out.println(bitmap.getWidth());
+////            img = preprocessImage(mat);
+////            saveImageToInternalStorage(bitmap,img,framecount,dir)
+//        }
+//
+//    }
 
 
 }
