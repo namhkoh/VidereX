160a6bee205ab3115c6d66663d7c6c7305c7a90a
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-05-03 10:54:59 +0900

    TensorFlow Lite model integration added.

--- app/src/main/java/com/kohdev/viderex/MenuActivity.java
+++ app/src/main/java/com/kohdev/viderex/MenuActivity.java
@@ -19,12 +19,16 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.Image;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
@@ -40,6 +44,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.google.gson.Gson;
@@ -72,8 +78,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * This activity will provide hte user with options to either follow a route or record a route.
@@ -106,11 +114,40 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
     ImageView testView;
     String json;
 
-    private Interpreter tfLite;
-    private static final int REQUEST_RECORD_AUDIO = 13;
+    private static final int SAMPLE_RATE = 16000;
+    private static final int SAMPLE_DURATION_MS = 1000;
+    private static final int RECORDING_LENGTH = (int) (SAMPLE_RATE * SAMPLE_DURATION_MS / 1000);
+    private static final long AVERAGE_WINDOW_DURATION_MS = 1000;
+    private static final float DETECTION_THRESHOLD = 0.50f;
+    private static final int SUPPRESSION_MS = 1500;
+    private static final int MINIMUM_COUNT = 3;
+    private static final long MINIMUM_TIME_BETWEEN_SAMPLES_MS = 30;
     private static final String LABEL_FILENAME = "file:///android_asset/conv_actions_labels.txt";
     private static final String MODEL_FILENAME = "file:///android_asset/conv_actions_frozen.tflite";
+
+    // UI elements.
+    private static final int REQUEST_RECORD_AUDIO = 13;
     private static final String LOG_TAG = MenuActivity.class.getSimpleName();
+
+    // Working variables.
+    short[] recordingBuffer = new short[RECORDING_LENGTH];
+    int recordingOffset = 0;
+    boolean shouldContinue = true;
+    private Thread recordingThread;
+    boolean shouldContinueRecognition = true;
+    private Thread recognitionThread;
+    private final ReentrantLock recordingBufferLock = new ReentrantLock();
+
+    private List<String> labels = new ArrayList<String>();
+    private List<String> displayedLabels = new ArrayList<>();
+    private RecognizeCommands recognizeCommands = null;
+
+    private Interpreter tfLite;
+
+    private long lastProcessingTimeMs;
+    private Handler handler = new Handler();
+    private HandlerThread backgroundThread;
+    private Handler backgroundHandler;
 
     //Android speech
     private TextToSpeech textToSpeech;
@@ -119,6 +156,7 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         OpenCVLoader.initDebug();
 
@@ -162,6 +200,40 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
 
         String actualLabelFilename = LABEL_FILENAME.split("file:///android_asset/", -1)[1];
         Log.e(LOG_TAG, "Reading labels from: " + actualLabelFilename);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(getAssets().open(actualLabelFilename)));
+            String line;
+            while ((line = br.readLine()) != null) {
+                labels.add(line);
+                if (line.charAt(0) != '_') {
+                    displayedLabels.add(line.substring(0, 1).toUpperCase() + line.substring(1));
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Problem reading label file!", e);
+        }
+
+        // Set up an object to smooth recognition results to increase accuracy.
+        recognizeCommands =
+                new RecognizeCommands(
+                        labels,
+                        AVERAGE_WINDOW_DURATION_MS,
+                        DETECTION_THRESHOLD,
+                        SUPPRESSION_MS,
+                        MINIMUM_COUNT,
+                        MINIMUM_TIME_BETWEEN_SAMPLES_MS);
+
+        String actualModelFilename = MODEL_FILENAME.split("file:///android_asset/", -1)[1];
+        try {
+            tfLite = new Interpreter(loadModelFile(getAssets(), actualModelFilename));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        tfLite.resizeInput(0, new int[]{RECORDING_LENGTH, 1});
+        tfLite.resizeInput(1, new int[]{1});
 
         if (routes == null) {
             routes = new HashMap<>();
@@ -175,114 +247,80 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
         prefs = getPreferences(Context.MODE_PRIVATE);
 
         //Speech block
-        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {
-                Log.d(TAG, "onReadyForSpeech");
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-                Log.d(TAG, "onBeginningfSpeech");
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-                Log.e(TAG, "this is on end of speech.");
-            }
-
-            @Override
-            public void onError(int i) {
-                Log.e(TAG, "on Error: " + i);
-
-            }
-
-            @Override
-            public void onResults(Bundle bundle) {
-                Log.e(TAG, "on Results");
-                //getting all the matches
-                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                Log.e("ALL MATCHES", matches.toString());
-
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-
-            }
-        });
-
+//        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+//        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+//            @Override
+//            public void onReadyForSpeech(Bundle bundle) {
+//                Log.d(TAG, "onReadyForSpeech");
+//            }
+//
+//            @Override
+//            public void onBeginningOfSpeech() {
+//                Log.d(TAG, "onBeginningfSpeech");
+//            }
+//
+//            @Override
+//            public void onRmsChanged(float v) {
+//
+//            }
+//
+//            @Override
+//            public void onBufferReceived(byte[] bytes) {
+//
+//            }
+//
+//            @Override
+//            public void onEndOfSpeech() {
+//                Log.e(TAG, "this is on end of speech.");
+//            }
+//
+//            @Override
+//            public void onError(int i) {
+//                Log.e(TAG, "on Error: " + i);
+//
+//            }
+//
+//            @Override
+//            public void onResults(Bundle bundle) {
+//                Log.e(TAG, "on Results");
+//                //getting all the matches
+//                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+//                Log.e("ALL MATCHES", matches.toString());
+//
+//            }
+//
+//            @Override
+//            public void onPartialResults(Bundle bundle) {
+//            }
+//
+//            @Override
+//            public void onEvent(int i, Bundle bundle) {
+//
+//            }
+//        });
+//
         speechButton.setOnTouchListener((view, motionEvent) -> {
             switch (motionEvent.getAction()) {
                 case MotionEvent.ACTION_UP:
-                    mSpeechRecognizer.stopListening();
+//                    mSpeechRecognizer.stopListening();
+                    stopRecognition();
+                    stopRecording();
                     break;
 
                 case MotionEvent.ACTION_DOWN:
-                    mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+//                    mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
                     System.out.println("listening");
+                    startRecording();
+                    startRecognition();
+
                     break;
             }
             return false;
         });
 
-//        // SPEECH TO TEXT END
-//
-//        textToSpeech = new TextToSpeech(getApplicationContext(), status -> {
-//
-//        });
-//
-//        //Results of pressing the speech button.
-//        textToSpeech.setOnUtteranceProgressListener(
-//                new UtteranceProgressListener() {
-//                    @Override
-//                    public void onStart(String utteranceId) {
-//                        speechButton.setEnabled(false);
-//                    }
-//
-//                    @Override
-//                    public void onDone(String utteranceId) {
-//                        if (index == list.size()) {
-//                            Log.d("Log", "Speach button deactivated");
-//                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    speechButton.setEnabled(false);
-//                                }
-//                            });
-//                        } else {
-//                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    speechButton.setEnabled(true);
-//                                }
-//                            });
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onError(String utteranceId) {
-//
-//                    }
-//                });
     }
 
 
@@ -321,9 +359,6 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
         //        startActivity(intent);
     }
 
-    public void loadSavedData() {
-
-    }
 
     private void uriToBitmap(Uri selectedFileUri) {
         try {
@@ -545,6 +580,192 @@ public class MenuActivity extends AppCompatActivity implements SensorEventListen
         long startOffset = fileDescriptor.getStartOffset();
         long declaredLength = fileDescriptor.getDeclaredLength();
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+
+    private void recognize() {
+
+        Log.v(LOG_TAG, "Start recognition");
+
+        short[] inputBuffer = new short[RECORDING_LENGTH];
+        float[][] floatInputBuffer = new float[RECORDING_LENGTH][1];
+        float[][] outputScores = new float[1][labels.size()];
+        int[] sampleRateList = new int[]{SAMPLE_RATE};
+
+        // Loop, grabbing recorded data and running the recognition model on it.
+        while (shouldContinueRecognition) {
+            long startTime = new Date().getTime();
+            // The recording thread places data in this round-robin buffer, so lock to
+            // make sure there's no writing happening and then copy it to our own
+            // local version.
+            recordingBufferLock.lock();
+            try {
+                int maxLength = recordingBuffer.length;
+                int firstCopyLength = maxLength - recordingOffset;
+                int secondCopyLength = recordingOffset;
+                System.arraycopy(recordingBuffer, recordingOffset, inputBuffer, 0, firstCopyLength);
+                System.arraycopy(recordingBuffer, 0, inputBuffer, firstCopyLength, secondCopyLength);
+            } finally {
+                recordingBufferLock.unlock();
+            }
+
+            // We need to feed in float values between -1.0f and 1.0f, so divide the
+            // signed 16-bit inputs.
+            for (int i = 0; i < RECORDING_LENGTH; ++i) {
+                floatInputBuffer[i][0] = inputBuffer[i] / 32767.0f;
+            }
+
+            Object[] inputArray = {floatInputBuffer, sampleRateList};
+            Map<Integer, Object> outputMap = new HashMap<>();
+            System.out.println(outputMap);
+            outputMap.put(0, outputScores);
+
+            // Run the model.
+            tfLite.runForMultipleInputsOutputs(inputArray, outputMap);
+
+            // Use the smoother to figure out if we've had a real recognition event.
+            long currentTime = System.currentTimeMillis();
+            final RecognizeCommands.RecognitionResult result =
+                    recognizeCommands.processLatestResults(outputScores[0], currentTime);
+            lastProcessingTimeMs = new Date().getTime() - startTime;
+            
+            System.out.println(result.foundCommand);
+
+//            if (!result.foundCommand.startsWith("_") && result.isNewCommand) {
+//                int labelIndex = -1;
+//                for (int i = 0; i < labels.size(); ++i) {
+//                    if (labels.get(i).equals(result.foundCommand)) {
+//                        labelIndex = i;
+//                    }
+//                }
+//                final String score = Math.round(result.score * 100) + "%";
+//                System.out.println(result.foundCommand + " " + score);
+//            }
+//            runOnUiThread(
+//                    new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            // If we do have a new command, highlight the right list entry.
+//                            if (!result.foundCommand.startsWith("_") && result.isNewCommand) {
+//                                int labelIndex = -1;
+//                                for (int i = 0; i < labels.size(); ++i) {
+//                                    if (labels.get(i).equals(result.foundCommand)) {
+//                                        labelIndex = i;
+//                                    }
+//                                }
+//                                final String score = Math.round(result.score * 100) + "%";
+//                            }
+//                        }
+//                    });
+            try {
+                Thread.sleep(MINIMUM_TIME_BETWEEN_SAMPLES_MS);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        Log.v(LOG_TAG, "End recognition");
+    }
+
+
+    public synchronized void startRecording() {
+        if (recordingThread != null) {
+            return;
+        }
+        shouldContinue = true;
+        recordingThread =
+                new Thread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                record();
+                            }
+                        });
+        recordingThread.start();
+    }
+
+    private void record() {
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
+
+        // Estimate the buffer size we'll need for this device.
+        int bufferSize =
+                AudioRecord.getMinBufferSize(
+                        SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
+            bufferSize = SAMPLE_RATE * 2;
+        }
+        short[] audioBuffer = new short[bufferSize / 2];
+
+        AudioRecord record =
+                new AudioRecord(
+                        MediaRecorder.AudioSource.DEFAULT,
+                        SAMPLE_RATE,
+                        AudioFormat.CHANNEL_IN_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT,
+                        bufferSize);
+
+        if (record.getState() != AudioRecord.STATE_INITIALIZED) {
+            Log.e(LOG_TAG, "Audio Record can't initialize!");
+            return;
+        }
+
+        record.startRecording();
+
+        Log.v(LOG_TAG, "Start recording");
+
+        // Loop, gathering audio data and copying it to a round-robin buffer.
+        while (shouldContinue) {
+            int numberRead = record.read(audioBuffer, 0, audioBuffer.length);
+            int maxLength = recordingBuffer.length;
+            int newRecordingOffset = recordingOffset + numberRead;
+            int secondCopyLength = Math.max(0, newRecordingOffset - maxLength);
+            int firstCopyLength = numberRead - secondCopyLength;
+            // We store off all the data for the recognition thread to access. The ML
+            // thread will copy out of this buffer into its own, while holding the
+            // lock, so this should be thread safe.
+            recordingBufferLock.lock();
+            try {
+                System.arraycopy(audioBuffer, 0, recordingBuffer, recordingOffset, firstCopyLength);
+                System.arraycopy(audioBuffer, firstCopyLength, recordingBuffer, 0, secondCopyLength);
+                recordingOffset = newRecordingOffset % maxLength;
+            } finally {
+                recordingBufferLock.unlock();
+            }
+        }
+
+        record.stop();
+        record.release();
+    }
+
+    public synchronized void startRecognition() {
+        if (recognitionThread != null) {
+            return;
+        }
+        shouldContinueRecognition = true;
+        recognitionThread =
+                new Thread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                recognize();
+                            }
+                        });
+        recognitionThread.start();
+    }
+
+    public synchronized void stopRecognition() {
+        if (recognitionThread == null) {
+            return;
+        }
+        shouldContinueRecognition = false;
+        recognitionThread = null;
+    }
+
+    public synchronized void stopRecording() {
+        if (recordingThread == null) {
+            return;
+        }
+        shouldContinue = false;
+        recordingThread = null;
     }
 
 
